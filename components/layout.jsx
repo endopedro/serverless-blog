e5b302763de8b0539a4927a6a4de2993a5ac9674
endop <endopedro@gmail.com>
2020-04-18 22:24:06 -0300

    mainNav component

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import MainNav from '@components/mainNav'
 
 export default ({ children }) => {
   return (
     <>
+      <MainNav />
       <div>
         <main>{children}</main>
       </div>
