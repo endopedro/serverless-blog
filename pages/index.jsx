6f1b2718e3356e01baa766ee20c7d810cb9fe3e9
Pedro <endopedro@gmail.com>
2020-05-15 15:07:39 -0300

    Add loading to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import ReactLoading from 'react-loading'
 
 import Layout from '@components/layout/layout'
 import PostCard from '@components/postCard'
@@ -11,11 +12,14 @@ const IndexPage = () => {
   }, [])
 
   const [posts, setposts] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const getPosts = async () => {
+    setLoading(true)
     const res = await fetch('/api/posts')
     const json = await res.json()
     setposts(json)
+    setLoading(false)
   }
 
   return (
@@ -25,6 +29,7 @@ const IndexPage = () => {
         <Container>
           <Row>
             {posts.map(post => (<Col lg={4} className="mb-4"><PostCard post={post}/></Col>))}
+            {loading ? <ReactLoading type="spin" color="#0D7EA6" className="my-5 mx-auto"/> : ''}
           </Row>
         </Container>
       </div>
